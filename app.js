592c5dfb9980f835dd556b1183d9f731ba62add0
Stuart Brown <jinky32@gmail.com>
2019-05-22 21:59:38 +0100

    added googleController to control the res.send

--- app.js
+++ app.js
@@ -4,8 +4,9 @@ var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
+const routes = require('./routes/index');
+// var indexRouter = require('./routes/index');
+// var usersRouter = require('./routes/users');
 
 var app = express();
 
@@ -19,8 +20,10 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/', indexRouter);
-app.use('/users', usersRouter);
+// After allllll that above middleware, we finally handle our own routes!
+app.use('/', routes);
+// app.use('/', indexRouter);
+// app.use('/users', usersRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
--- routes/index.js
+++ routes/index.js
@@ -1,14 +1,16 @@
 var express = require('express');
 var router = express.Router();
 
+//import controllers
+const googleController = require('../controllers/googleController');
+
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', { title: 'Express stu' });
 });
 
-router.get('/ga', function(req, res, next) {
-  // res.render('index', { title: 'Express' });
-  res.send('WOOTS');
-});
+router.get('/ga',
+  googleController.hello
+);
 
 module.exports = router;
