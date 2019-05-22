f0e23e7634ba03a887d8181d88860222069edea6
Stuart Brown <jinky32@gmail.com>
2019-05-22 22:08:43 +0100

    move google assistant intents into googleController

--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,18 @@
 var express = require('express');
 var router = express.Router();
 
+// Import the Dialogflow module and response creation dependencies from the
+// Actions on Google client library.
+const {
+  dialogflow,
+  Permission,
+  Suggestions,
+  BasicCard,
+} = require('actions-on-google');
+
+// Instantiate the Dialogflow client.
+const googleApp = dialogflow({debug: true});
+
 //import controllers
 const googleController = require('../controllers/googleController');
 
