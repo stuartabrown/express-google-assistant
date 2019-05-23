9185854c277a72a0b53ec62f180e3c48c1aede4b
Stuart Brown <jinky32@gmail.com>
2019-05-23 17:09:28 +0100

    added intent to index

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,26 @@
 var express = require('express');
 var router = express.Router();
+const {
+  dialogflow,
+  Permission,
+  Suggestions,
+  BasicCard,
+} = require('actions-on-google');
+
+// Instantiate the Dialogflow client.
+const googleApp = dialogflow({debug: true});
+
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  // res.render('index', { title: 'Express' });
+  // Handle the Dialogflow intent named 'favorite color'.
+// The intent collects a parameter named 'color'.
+  app.intent('favorite color', (conv, {color}) => {
+    const luckyNumber = color.length;
+    // Respond with the user's lucky number and end the conversation.
+    conv.close('Your lucky number is ' + luckyNumber);
+  });
 });
 
 router.get('/ga', function(req, res, next) {
